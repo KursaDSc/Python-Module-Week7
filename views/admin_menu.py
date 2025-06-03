12019a5f5f2faa1aece8409e0877a829922ff338
Kursad <122346253+KursaDSc@users.noreply.github.com>
2025-06-03 19:29:34 +0200

    .

--- views/admin_menu.py
+++ views/admin_menu.py
@@ -98,7 +98,8 @@ class AdminMenuWindow(QWidget):
 
 
 if __name__ == "__main__":
-    import sys
+    import sys, os
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
     app = QApplication(sys.argv)
     window = AdminMenuWindow()
     window.show()
